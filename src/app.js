38393a0b4699314ff6d766ee50ffb33f11bcd35b
David Zhang <david@eventmobi.com>
2015-10-22 00:35:59 -0400

    Initial Commit
    Basic app design, will add more features on top

---
+++ src/app.js
@@ -0,0 +1,52 @@
+/**
+ * PebbleReact - Test your reaction!
+ *
+ * First Pebble WatchApp made by David Zhang
+ */
+
+var UI = require('ui');
+
+var main = new UI.Card({
+    title: 'PebbleReact',
+    body: 'Press select to begin!'
+});
+
+var wait = new UI.Card({
+    subtitle: 'Wait..'
+});
+
+var response = new UI.Card({
+    subtitle: 'Click select now!'
+});
+
+main.show();
+
+main.on('click', 'select', function(e) {
+    var waitTime = parseInt(Math.random() * 5000);
+    main.hide();
+    wait.show();
+    setTimeout(function() {
+        var d = new Date();
+        var preResponseTime = d.getTime();
+        wait.hide();
+        response.show();
+        response.on('click', 'select', function(e) {
+            var d2 = new Date();
+            var postResponseTime = d2.getTime();
+            var difference = postResponseTime - preResponseTime;
+            
+            var result = new UI.Card({
+                title: 'Results',
+                subtitle: 'Your response time is: ' + difference + 'ms'
+            });
+            response.hide();
+            result.show();
+            
+            result.on('click', 'select', function(e) {
+                result.hide();
+                main.show(); 
+            });
+        });
+    }, waitTime);
+    
+});
